3fbe7b05d9e0e7a7c8166229dc45f225744977a6
Lawrence Agyei <lawgotcode1990@gmail.com>
2018-04-30 11:07:01 -0500

    api

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ import Weather from "./components/Weather";
 const API_KEY ="3ae07f75cf8a3df0bf8ac182469709af";
 
 class App extends React.Component {
-  getWeather = async () => {
+  getWeather = async (e) => {
+    e.preventDefault();
     const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=Chicago,us&appid=${API_KEY}`);
       const data = await api_call.json();
       console.log(data);
@@ -16,7 +17,7 @@ class App extends React.Component {
     return (
       <div>
        <Titles />
-       <Form />
+       <Form getWeather={this.getWeather}/>
        <Weather />
       </div>
       
