d7a9d219ca178c3f4b5c85e9c587d404a5d97e30
Lawrence Agyei <lawgotcode1990@gmail.com>
2018-05-14 18:17:19 -0500

    states

--- src/App.js
+++ src/App.js
@@ -22,22 +22,22 @@ class App extends React.Component {
     const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}`);
       const data = await api_call.json();
       if (city && country) {
-        console.log(data);
+        
       this.setState({
           temperature: data.main.temp,
           city: data.name,
           country: data.sys.country,
-          humidity: data.main.humudity,
+          humidity: data.main.humidity,
           description: data.weather[0].description,
           error: ""
       });
     } else {
       this.setState({
-          temperature: data.main.temp,
-          city: data.name,
-          country: data.sys.country,
-          humidity: data.main.humudity,
-          description: data.weather[0].description,
+          temperature: undefined,
+          city: undefined,
+          country: undefined,
+          humidity: undefined,
+          description: undefined,
           error: "Please enter the value."
         });
     }
--- src/components/Titles.js
+++ src/components/Titles.js
@@ -1,20 +1,12 @@
 import React from "react";
 
 
-class Titles extends React.Component {
-	render() {
-		return (
-			<div>
-                <h1>Weather Finder</h1>
-
-                <p>Find out tempareture, conditions and more...</p>
-
-
-			</div>
-			);
-	}
-};
-
+const Titles = () => (
+	<div>
+        <h1>Weather Finder</h1>
+       <p>Find out tempareture, conditions and more...</p>
+  </div>
+	);
 
 
 export default Titles;
